d5a53a8400557e549406cc64a2039b9d9502db74
Cao Anh Quan <aquancva@gmail.com>
2017-06-30 23:23:18 +0700

    add basic routing and controller

---
+++ controllers/ProductController.js
@@ -0,0 +1,25 @@
+const pool = require('../config/database');
+
+module.exports = {
+    products: function (req, res) {
+        const sql = 'select * from products order by created_at limit 10 offset 0';
+        pool.query(sql, function (error, rows, fields) {
+            if (error) throw error;
+            res.json({
+                products: rows.map(function (r) {
+                    return {
+                        id: r.id,
+                        created_at: r.created_at,
+                        image_url: r.image_url,
+                        views_count: r.views,
+                        thumb_name: r.thumb_name,
+                        thumb_url: r.thumb_url
+                    }
+                })
+            });
+            // console.log(results);
+            // console.log(fields);
+        });
+        // res.send('Hello world');
+    }
+};
