fe0d25b46d79a3040959ca60946154509f4b8ae4
Cao Anh Quan <aquancva@gmail.com>
2017-07-01 19:00:22 +0700

    time elapse

--- util/helper.js
+++ util/helper.js
@@ -28,24 +28,24 @@ module.exports = {
         let interval = Math.floor(seconds / 31536000);
 
         if (interval > 1) {
-            return interval + "năm";
+            return interval + "năm trước";
         }
         interval = Math.floor(seconds / 2592000);
         if (interval > 1) {
-            return interval + " tháng";
+            return interval + " tháng trước";
         }
         interval = Math.floor(seconds / 86400);
         if (interval > 1) {
-            return interval + " ngày";
+            return interval + " ngày trước";
         }
         interval = Math.floor(seconds / 3600);
         if (interval > 1) {
-            return interval + " giờ";
+            return interval + " giờ trước";
         }
         interval = Math.floor(seconds / 60);
         if (interval > 1) {
-            return interval + " phút";
+            return interval + " phút trước";
         }
-        return Math.floor(seconds) + " giây";
+        return Math.floor(seconds) + " giây trước";
     }
 };
