1e5917340504ff048ded200bb48fa60f15384777
Cao Anh Quan <aquancva@gmail.com>
2017-07-01 12:14:13 +0700

    add CORS

--- app/app.js
+++ app/app.js
@@ -3,7 +3,36 @@ require('dotenv').config();
 const productsRouter = require('../routes/products');
 const publicRouter = require('../routes/public');
 
+
+const origins = [
+    "http://localhost:3000",
+    "http://colorme.vn",
+    "http://www.colorme.vn",
+    "http://manage.colorme.vn",
+    "http://beta.colorme.vn",
+    "http://colorme.dev"
+];
 const app = express();
+
+// Add headers
+app.use(function (req, res, next) {
+    if (origins.indexOf(req.headers.origin) > -1) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use('/products', productsRouter);
 app.use('/public', publicRouter);
