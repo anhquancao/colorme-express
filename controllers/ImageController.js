c5e36a0d4349a50780b2bde55a6876bd38e82e51
Cao Anh Quan <aquancva@gmail.com>
2017-07-18 11:21:21 +0700

    up

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -8,48 +8,56 @@ module.exports = {
         let width = 500;
         let height = 500;
 
-        if (req.query.width) {
-            width = Number(req.query.width);
-        }
-
-        if (req.query.height) {
-            height = Number(req.query.height);
-        }
-
-        const fileName = file.filename;
-
-        const date = new Date();
-        const photoKey = "tmp/" + fileName + date.getMilliseconds() + ".jpg";
-        // file.originalname.split(".").pop();
-        // fs.createReadStream(file.path)
-        sharp(file.path)
-            .resize(width, height)
-            .max()
-            .toBuffer()
-            .then(function (outputBuffer) {
-                S3.upload({
-                    Key: photoKey,
-                    Body: outputBuffer,
-                    ContentType: "image/jpeg",
-                    ACL: 'public-read'
-                }, function (err, data) {
-                    fs.unlink(file.path);
-                    if (err) {
-                        return console.log(err);
-                    }
-                    res.json({
-                        status: 1,
-                        data: Object.assign({}, data, {
-                            photo_key: photoKey,
-                            url: process.env.S3_URL + "/" + photoKey
-                        })
-                    });
-                });
+        if (file.size > 5000000) {
+            res.json({
+                status: 0,
+                message: "Dung lượng ảnh không được vượt quá 5Mb"
             })
-            .catch((error) => {
-                console.log(error);
-                res.json(error);
-            });
+        } else {
+            if (req.query.width) {
+                width = Number(req.query.width);
+            }
+
+            if (req.query.height) {
+                height = Number(req.query.height);
+            }
+
+            const fileName = file.filename;
+
+            const date = new Date();
+            const photoKey = "tmp/" + fileName + date.getMilliseconds() + ".jpg";
+            // file.originalname.split(".").pop();
+            // fs.createReadStream(file.path)
+            sharp(file.path)
+                .resize(width, height)
+                .max()
+                .toBuffer()
+                .then(function (outputBuffer) {
+                    S3.upload({
+                        Key: photoKey,
+                        Body: outputBuffer,
+                        ContentType: "image/jpeg",
+                        ACL: 'public-read'
+                    }, function (err, data) {
+                        fs.unlink(file.path);
+                        if (err) {
+                            return console.log(err);
+                        }
+                        res.json({
+                            status: 1,
+                            data: Object.assign({}, data, {
+                                photo_key: photoKey,
+                                url: process.env.S3_URL + "/" + photoKey
+                            })
+                        });
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.json(error);
+                });
+
+        }
 
     }
 };
