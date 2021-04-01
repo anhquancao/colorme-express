7c507a343d0a64e695a52dd35540267d502813d1
hieu <goldenrezgun@gmail.com>
2021-04-01 11:14:01 +0700

    more product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -456,8 +456,8 @@ module.exports = {
 
 
                                 pool.query({sql:'select * from products ' +
-                                        'join users on users.id = products.author_id where author_id=' + product.author_id +
-                                        " and products.id != " + product.id + " order by RAND() limit 5", nestTables: true},
+                                        'join users on users.id = products.author_id where rating >= 20 and author_id=' + product.author_id +
+                                        " and products.id != " + product.id + " order by RAND() limit 4", nestTables: true},
                                     function (error, result, fields) {
                                     if (error) return console.log(error);
 
