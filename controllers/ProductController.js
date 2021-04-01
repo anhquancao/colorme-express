647518deb8ef5502fe2487f41f91252d7e48de11
hieu <goldenrezgun@gmail.com>
2021-04-01 15:00:09 +0700

    more product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -340,7 +340,7 @@ module.exports = {
     weekRating:function (req, res) {
         const start_time = req.query.start_time;
         const end_time = req.query.end_time;
-        let sql = `select id from products where DATE(created_at) >= '${start_time}' and DATE(created_at) <= '${end_time}' order by rating desc`;
+        let sql = `select id from products where DATE(created_at) >= '${start_time}' and DATE(created_at) <= '${end_time}' order by rating desc , created_at desc`;
         const options = {sql, nestTables: true};
         pool.query(options, function (error, rows, fields) {
                 let weekRating = rows.map(row=>{
